bcc88cd19e5335e3135a84e15024b371554373b8
CireWire <djcire956@gmail.com>
2018-06-19 23:41:28 -0500

    Create greeting.cs

---
+++ greeting.cs
@@ -0,0 +1,14 @@
+using System;
+
+class mainClass
+{
+  public static void Greet (string x)
+  {
+    Console.WriteLine(x);
+  }
+
+
+  public static void Main (string[] args) {
+    Greet("Hello World!");
+  }
+}
